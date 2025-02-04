6c302df470447f7abb750595c622d0ac4b5f3978
AhmedNoaman <ahmed.noaman@rightechs.net>
2025-02-04 16:16:13 +0200

    solve issues when create patch delay

--- elsaka_hr_penalty/hr_penalty.py
+++ elsaka_hr_penalty/hr_penalty.py
@@ -219,7 +219,7 @@ class patch_delay_cal(models.Model):
                 employee_delay_id = employee_delay_pool.create(emp_delay_data)
 
                 # CALL CALCULATION METHOD FOR EACH EMPLOYEE
-                employee_delay_pool.calc_delay()
+                employee_delay_id.calc_delay()
 
     def get_employee_shift(self, employee_id, date_from, date_to):
         employee_pool = self.env['hr.employee']
