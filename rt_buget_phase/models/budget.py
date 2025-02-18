e567f7722e6a21fe63c1f3ac71d26442b35139e5
mahmoudraafatahmed <mahmoudahmed21101@gmail.com>
2025-02-18 15:52:20 +0200

    rt_budget_phase had been developed for making budget_lines readonly when state in done or cancel and adding statr in list view of budget and budget_lines for mod test developed07 180225

--- rt_buget_phase/models/budget.py
+++ rt_buget_phase/models/budget.py
@@ -208,7 +208,7 @@ class BudgetLine(models.Model):
 
     task_id = fields.Many2one('project.task', 'Task', copy=False, store=True)
     project_id = fields.Many2one(related='budget_id.project_id', copy=False, store=True, precompute=True)
-    sale_order_id = fields.Many2one(related='budget_id.sale_order_id', copy=False, store=True)
+    sale_order_id = fields.Many2one(related='budget_id.sale_order_id', copy=False, store=True, precompute=True)
     product_id = fields.Many2one('product.product', string="Product")
     phase_id = fields.Many2one('project.phase', 'Phase', compute="_compute_phase_id", copy=False, store=True, precompute=True)
     # phase_id = fields.Many2one(related='budget_id.phase_id', copy=False, store=True)
